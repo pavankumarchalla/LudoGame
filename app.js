d8d72040350fd91c67b4bc7dfb76e683d53b90ba
Pavan Kumar C <pavan.challa23@gmail.com>
2018-11-22 13:02:17 +0530

    roll dice functionality implemented

--- app.js
+++ app.js
@@ -11,13 +11,20 @@ GAME RULES:
 
 
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1
+document.querySelector('.dice').style.display = 'none';
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+document.querySelector('.btn-roll').addEventListener('click', function() {
 
+	var dice = Math.floor(Math.random() * 6) + 1;
+
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png'
+
+});

ab39cdbd2a71c62784ed177ab7127899c8eb127f
Pavan Kumar C <pavan.challa23@gmail.com>
2018-11-22 16:09:41 +0530

    Updating scores and changing the active player

--- app.js
+++ app.js
@@ -29,6 +29,27 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 	var diceDOM = document.querySelector('.dice');
 	diceDOM.style.display = 'block';
-	diceDOM.src = 'dice-' + dice + '.png'
+	diceDOM.src = 'dice-' + dice + '.png';
+
+	if (dice !== 1) {
+		
+		if (roundScore === 0) {
+			document.querySelector('.dice').style.display = 'block'
+		}
+
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+
+		document.getElementById('current-0').textContent = 0;
+		document.getElementById('current-1').textContent = 0;
+
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+
+		document.querySelector('.dice').style.display = 'none'
+	}
 
 });

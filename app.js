9663e534dc6f6e4973a16c1dddd6be1995128a78
Pavan Kumar C <pavan.challa23@gmail.com>
2018-11-22 13:07:18 +0530

    Player scores defaulted to 0

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ roundScore = 0;
 activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 
